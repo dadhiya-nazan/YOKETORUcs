dc225825eb6c334b910a6a127c42409174131f1a
橘朔也 <167387830+dadhiya-nazan@users.noreply.github.com>
2024-06-21 16:05:43 +0900

    GameUpdate

--- YOKETORUcs/Form1.Designer.cs
+++ YOKETORUcs/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            labelTitle = new Label();
+            buttonStart = new Button();
+            labelGameover = new Label();
+            buttonToTitle = new Button();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 50;
+            timer1.Tick += timer1_Tick;
+            // 
+            // labelTitle
+            // 
+            labelTitle.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            labelTitle.AutoSize = true;
+            labelTitle.Font = new Font("Yu Gothic UI", 72F, FontStyle.Bold, GraphicsUnit.Point);
+            labelTitle.ForeColor = Color.FromArgb(192, 255, 255);
+            labelTitle.Location = new Point(191, 135);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(542, 128);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "YOKETORU";
+            labelTitle.Click += labelTitle_Click;
+            // 
+            // buttonStart
+            // 
+            buttonStart.BackColor = Color.Cyan;
+            buttonStart.Font = new Font("UD デジタル 教科書体 NK-B", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            buttonStart.ForeColor = SystemColors.ButtonFace;
+            buttonStart.Location = new Point(401, 349);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(154, 84);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "スタート";
+            buttonStart.UseVisualStyleBackColor = false;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // labelGameover
+            // 
+            labelGameover.AutoSize = true;
+            labelGameover.Font = new Font("Bauhaus 93", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            labelGameover.Location = new Point(257, 180);
+            labelGameover.Name = "labelGameover";
+            labelGameover.Size = new Size(441, 73);
+            labelGameover.TabIndex = 2;
+            labelGameover.Text = "GAME OVER…";
+            // 
+            // buttonToTitle
+            // 
+            buttonToTitle.BackColor = Color.LightCoral;
+            buttonToTitle.Font = new Font("UD デジタル 教科書体 NK-B", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            buttonToTitle.ForeColor = SystemColors.ActiveCaptionText;
+            buttonToTitle.Location = new Point(401, 349);
+            buttonToTitle.Name = "buttonToTitle";
+            buttonToTitle.Size = new Size(154, 84);
+            buttonToTitle.TabIndex = 3;
+            buttonToTitle.Text = "タイトルへ";
+            buttonToTitle.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(944, 501);
+            Controls.Add(buttonStart);
+            Controls.Add(labelTitle);
+            Controls.Add(labelGameover);
+            Controls.Add(buttonToTitle);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label labelTitle;
+        private Button buttonStart;
+        private Label labelGameover;
+        private Button buttonToTitle;
     }
 }
--- YOKETORUcs/Form1.cs
+++ YOKETORUcs/Form1.cs
@@ -2,9 +2,85 @@ namespace YOKETORUcs
 {
     public partial class Form1 : Form
     {
+        //状態遷移、状態定義
+        enum State
+        {
+            None = -1,
+            Title,
+            Game,
+            Gameover,
+            Clear,
+        }
+
+        //<summary>
+        //次に切り替えたい状態
+        //<summary>
+        State nextState = State.Title;
+
+        //<summary>
+        //現在の状態
+        //<summary>
+        State currentState = State.None;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            InitState();
+            UpdateState();
+        }
+
+        void InitState()
+        {
+            if (nextState == State.None)
+            {
+                return;
+            }
+            currentState = nextState;
+            nextState = State.None;
+
+            //初期化処理
+            switch (currentState)
+            {
+                case State.Title:
+                    labelGameover.Visible = false;
+                    buttonToTitle.Visible = false;
+                    break;
+
+                case State.Game:
+                    labelTitle.Visible = false;
+                    buttonStart.Visible = false;
+                    break; 
+            }
+        }
+
+        //状態の更新メソッド
+        void UpdateState()
+        {
+            switch (currentState)
+            {
+                case State.Game:
+                    UpdateGame();
+                    break;
+            }
+        }
+        //ゲーム状態の更新用メソッド
+        void UpdateGame()
+        {
+
+        }
+
+        private void labelTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            nextState = State.Game;
         }
     }
 }
